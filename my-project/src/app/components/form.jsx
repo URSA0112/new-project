60e3906fdd51f8ef200b9102157d4ae43bed9868
URSA0112 <Nimone99@gmail.com>
2025-02-10 13:05:45 +0800

    new project weather started

---
+++ my-project/src/app/components/form.jsx
@@ -0,0 +1,9 @@
+export function Form(){
+    return(
+        <div className="bg-blue-300 w-[100%] h-[100vh]">
+            <input type="text" placeholder="Search"/>
+            <div className="bg-slate-50 w-10 h-20"></div>
+
+        </div>
+    )
+}
