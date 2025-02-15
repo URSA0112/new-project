89582f4fda54bc0773d60d7df98b4e9290a6eb84
URSA0112 <Nimone99@gmail.com>
2025-02-15 19:43:17 +0800

    happy seachbar almost done

--- weather/src/app/components/SearchBar.js
+++ weather/src/app/components/SearchBar.js
@@ -1,16 +1,17 @@
 export function SearchBar(props) {
+
     return (
         <div className="z-[5] absolute ml-[50%] translate-x-[-50%] 
-        w-[100px] h-[40px] sm:w-[250px] sm:h-[40px] md:w-[300px] md:h-[50px] lg:w-[400px] lg:h-[50px]
+        w-[200px] h-[40px] sm:w-[250px] sm:h-[40px] md:w-[300px] md:h-[50px] lg:w-[400px] lg:h-[50px]
         mt-[4%]  bg-[rgba(246,110,110,0.6)] rounded-full">
-            <input value={props.inputText} onChange={props.handleChange}
+            <input value={props.inputText} onChange={props.onChangeInput}
             type="text" placeholder="Search"
 
-            className="w-[100px] h-[10px] sm:w-[250px] sm:h-[10px] md:w-[300px] md:h-[50px] lg:w-[400px] lg:h-[50px] 
+            className="w-[200px] h-[10px] sm:w-[250px] sm:h-[10px] md:w-[300px] md:h-[50px] lg:w-[400px] lg:h-[50px] 
             bg-transparent
             rounded-3xl p-5" />
-             <button onClick={props.check} className="bg-red-600 w-20 h-10 rounded-full  absolute ml-[30%] mt-[2%] translate-x-[-50%] ">check</button>
-            <ul className="">{props.locationList}</ul>
+         
+            <ul  >{props.locationList}</ul>
             
         </div>
     )
--- weather/src/app/page.js
+++ weather/src/app/page.js
@@ -3,50 +3,48 @@ import { LightLeft } from "./components/LeftLayout";
 import { DarkRight } from "./components/RightLayout";
 import { SearchBar } from "./components/SearchBar";
 import { useState, useEffect } from "react";
-import chalk from "chalk";
 
-const listStyle= "bg-black/30 backdrop-sepia-50 pl-5 w-[400px] h-[30px] text-white"
-const dayTemp = "-30"
 export default function Home() {
-  const [inputText, setInputtext] = useState("")
-  function onChange(e) { setInputtext(e.target.value) }
-  const [locationArr, setLocationArr] = useState([])
+  const [inputText, setInputText] = useState("")
+  function handleInputChange(e) { setInputText(e.target.value) }
   const [locationList, setLocationList] = useState([])
-  const [filtered, setFiltered] = useState([])
-
-
-
-function handleClickOnLocation(){
-
-}
-
-function check(){     
-   setLocationList(filtered.map((each, index) => <div className={listStyle} onClick={handleClickOnLocation} key={index}>{each}</div>))
-}
+  const [locationData, setLocationData] = useState([])
 
   useEffect(() => {
     async function getWeather() {
       const response = await fetch("https://countriesnow.space/api/v0.1/countries")
-      const fetchedData = await response.json()
-      const dataArr = fetchedData.data
-      const country = dataArr.map(each => each.country)
-      const locationForm = dataArr.flatMap(each => each.cities.map(city => `${city} ${each.country}`));
-      setLocationArr(locationForm)
-      setFiltered(locationForm.filter(each => each.toLowerCase().slice(0,100).includes(inputText.toLowerCase())))
+      const Data = await response.json()
+      const countries = Data.data.map(each => each.countries)
+      const locations = Data.data.flatMap(each => each.cities.map(city => `${city} ${each.country}`))
+      setLocationData(locations)
     }
     getWeather()
-  }, [check]
-  )
+  }, [])
+
+  useEffect(() => {
+    if (inputText.trim() === "") { setLocationList([]); return; }
+    const filteredList = ((locationData
+      .filter(location => location.toLowerCase().trim().includes(inputText.toLowerCase().trim()))
+      .slice(0, 7)))
+      .map((each, index) => <div key={index} onClick={()=>selectedList(each,index)}  className="cursor-pointer hover:scale-105 hover:bg-white-100 hover:shadow-md transition-all duration-300 rounded-md p-2"
+      >{each}</div>)
+
+    setLocationList(filteredList)
+
+  }, [inputText])
+
+  function selectedList(each, index) {
+   console.log(each)
+  }
 
   return (
     <div>
-      <SearchBar inputText={inputText} handleChange={onChange} check={check} locationList={locationList}> </SearchBar>
-      <div className="flex min-w-[470px] min-h-[300px]"> 
-        <LightLeft dayTemp={dayTemp}/>
+      <SearchBar inputText={inputText} onChangeInput={handleInputChange} locationList={locationList}
+        eachList={selectedList}> </SearchBar>
+      <div className="flex min-w-[470px] min-h-[300px]">
+        <LightLeft />
         <DarkRight />
       </div>
-     
-
     </div>
   )
 }
